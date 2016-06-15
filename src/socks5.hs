e6950ea5260ad4b1db549dec9083b090282d6c3d
Erèbe <romain.gerard@erebe.eu>
2016-06-15 14:01:50 +0200

    maj sock5

--- src/socks5.hs
+++ src/socks5.hs
@@ -9,7 +9,8 @@ module Socks5 where
 
 
 import           ClassyPrelude
-import qualified Data.Binary.Get as Bin
+import           Data.Binary
+import           Data.Binary.Get
 import           Network.Socket  (HostName, PortNumber)
 
 data AuthMethod = NoAuth
@@ -23,6 +24,40 @@ data RequestAuth = RequestAuth
   { version :: Int
   , methods :: Vector AuthMethod
   } deriving (Show, Read)
+
+instance Binary AuthMethod where
+  put val = case val of
+    NoAuth -> putWord8 0x00
+    GSSAPI -> putWord8 0x01
+    Login -> putWord8 0x02
+    NotAllowed -> putWord8 0xFF
+    _ {- Reserverd -} -> putWord8 0x03
+
+  get = do
+    method <- getWord8
+    return $ case method of
+      0x00 -> NoAuth
+      0x01 -> GSSAPI
+      0x02 -> Login
+      0xFF -> NotAllowed
+      _ -> Reserved
+
+
+instance Binary RequestAuth where
+  put RequestAuth{..} = do
+    putWord8 (fromIntegral version)
+    putWord8 (fromIntegral $ length methods)
+    sequence_ ( put <$> methods)
+
+  get = do
+    version <- fromIntegral <$> getWord8
+    guard (version == 0x05)
+    nbMethods <- fromIntegral <$> getWord8
+    guard (version <= 0xFF)
+    methods <- replicateM nbMethods get
+    return $ RequestAuth version methods
+
+
 
 
 data Request = Request
