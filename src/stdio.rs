aca065fcab27aec2472669f50296aebb93259226
Σrebe - Romain GERARD <erebe@erebe.eu>
2023-10-17 00:00:45 +0200

    Bump
    Former-commit-id: a5c765b81ecdd7a4a64ae9a29ac827f054dc0f6b [formerly 303f4a61e4c6b8146afa3035fd79c7ea9f4c3093] [formerly 514ae96091247299929d298c96fb0898b5c94bf2 [formerly 3496804edf70a12aa77173061c40f36d9301d58c]]
    Former-commit-id: c344f3736face3ce16e7fda6e395f63adf864725 [formerly f82d31b14bf430666762dd5c4936d5a3107a0d17]
    Former-commit-id: dc05fa123697b2011a8c8e77bd7c3cb94f6fa30f
    Former-commit-id: 55513ef82602cc7eba5f9979282c3c0aa750ebba
    Former-commit-id: 64bbd9b657056f122a10dd9d83fc3900f1f311a4
    Former-commit-id: ef78d4ab69ea339f99878c8f3cc649c4f39a1f96 [formerly 252cf5bb9484db648812603dffb0cb686fed0aa3]
    Former-commit-id: 583caf5e4d75622fb34badbec536acde3f2b4c58

--- src/stdio.rs
+++ src/stdio.rs
@@ -1,8 +1,7 @@
 use tokio_fd::AsyncFd;
-use tracing::info;
 
 pub async fn run_server() -> Result<(AsyncFd, AsyncFd), anyhow::Error> {
-    info!("Starting STDIO server");
+    eprintln!("Starting STDIO server");
 
     let stdin = AsyncFd::try_from(libc::STDIN_FILENO)?;
     let stdout = AsyncFd::try_from(libc::STDOUT_FILENO)?;
