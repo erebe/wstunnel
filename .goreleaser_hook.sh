11b6f026821fcb02996d4f04d2807c22356fd979
Σrebe - Romain GERARD <erebe@erebe.eu>
2023-10-18 16:54:09 +0200

    Bump
    Former-commit-id: f121b9faa72b5c3e258e518bc402b1426fc1e4bc [formerly 57242cfdb00382e09adf4aa3faab128c2cbb3b56] [formerly 14d41558d791ce91b29bf2729751fe89300eb1cd [formerly d2a0755561566363ef19322eed7187639e850f87]] [formerly 7ef6b8d94f72d6533e3a417f7bb1194f1db3c703 [formerly 437954cc027504d5da376ca4d1f870af1166f969] [formerly a93aa82285e1244ebec8c5b991875431d43a8168 [formerly 2b41975cd486b957f205bec1bd82f77edd9f9eeb]]] [formerly fd56545f3b616b0fe4b6f729f983f0bcc0ce89c8] [formerly 4e5d1fb92818afcd34a7e2c22d6e6b4687cfe03c] [formerly dbbe149821aefa3cb7f458385ccd94ce77b1008f] [formerly c7221d2f251753448b9c8e8a5c870810b99d492f] [formerly f37b940f23cb44d1c7f38ffa46a342e44bafacc7 [formerly 324e770cb1b2e3afd54540d3c3f0bf722d8891f3] [formerly c7221d2f251753448b9c8e8a5c870810b99d492f [formerly bf5001403ab23d9359bc9d4e6e0f0b1d2c75a328 [formerly 677bb1f3eabeb3f19b1ed9285adb3931d9a7f2f4] [formerly f938dd89f9bf3d9cd3686aef89fb633094cd0088 [formerly 9595f8daf0f05feeedbd8eb2be650e697f520116]] [formerly 817af417c7b620b3b15f50fb076455202308000d [formerly 27d2cc7478a6b601091e6b43cc5a77e8075fffbd] [formerly 6765a79647b4616cb4ab2270f14b01ec5ee1e64e [formerly 6314bd56ac4bc8b8c5e4404b26474c59f63a67e4]]] [formerly 5ea89c6d4bc4897a9eb4abc77b4a9b21f155c5a0 [formerly 7c6999784c205eac69441f1ba71ddf9f87b67ed7] [formerly 8955641c3e3a7a0cf558512784f5294628e20105 [formerly 07922e3cbc8e1fc38f54a69e3ccc3533ffa94003]] [formerly 928f611a866ba550ff3cf490339a3a649f72d294 [formerly 2d0b4f65523fcfe2df4bc058c4fb4beea76aa261] [formerly a8d331a021d274cb8ecf5bebb7394c4bddb34e74 [formerly ab4b0438db52fc2d47e2c5f4735e98374333c8e7]]]] [formerly 5dc739a5541045de1dcec464fbe3ed18870084a1]]]] [formerly e8c7d65d4f9c6a386ba5adc8c9111d53076d9cbb] [formerly 840d988cba7c08ab0f596d7b699c240ab09d8eb1] [formerly af2fccde8822ff106b887835692de63c43d3d212 [formerly 183f377e83505f5f072361f44e7ff4d38c25abee]] [formerly 3804e8f1ee3d9752745ca811d78300e6f4a13c83 [formerly 2cdcdefe21b1f8537378c58995c3ed9aa6891e0d] [formerly ad0d389ba3f21f614581c0ea51e1fa4701cb629f [formerly 4c41fadda7d5b69473461f28a14e0ebdbb737f9f]]]
    Former-commit-id: 275695df10620d1788e85788e77de9368e3bc9ac [formerly 372da06a9755c57ee06f5d4d449caba5e8355ebf] [formerly ffe51bd363168adce7d12bd45e00de688ea2ca93 [formerly 6d7e2380f14d74aa9119bf099f911e0ccf9258c0]]
    Former-commit-id: 5fb5db24adccece53a52f29ad512107805f073a6 [formerly 35de7e910918a0d704ed9f7b36eacaa74e00a9f2]
    Former-commit-id: fad9fd0ef0ee514bd404e3ec7206411cb73261e3
    Former-commit-id: c533c906e8a91d29f8d9e9e9245b2fd77fbf5b05
    Former-commit-id: 1a0d311c288c79240e887c9661a0a140417c94a2 [formerly 9122709836e043345b586c574a78fb88cc2a7e07] [formerly c8dc773ce17d899b111be8acf472442d364930d1 [formerly da11462ee51c52389c7cf94ab127b36a3cde7d6f]]
    Former-commit-id: e2b5eecad95857968a6166378ee1046362fc09db [formerly c8dc773ce17d899b111be8acf472442d364930d1]
    Former-commit-id: 5e0353c54a129b3895b35235f90f6534f8f4a1be
    Former-commit-id: c534117d9be6615fa8e8222bbcdab7a917ac15dd
    Former-commit-id: c4cf0387fe323776e4815e42d9bd93d28b714588
    Former-commit-id: cc204f08cdc8bb36d8b173762456fb7290b5ab0e [formerly 5c840360606a528da068591fda7227a1c8546e3f] [formerly 8f1a97740d271ab22d4d44f925fce20cb6c19cdf [formerly f35449ba7e29d3ba47100c6bf01518f1e37840ce]]
    Former-commit-id: e70fa9585e864bb322bdb6c6c348bd6c1e9ed430 [formerly 36e4d5ae51ef31d95874066620444ee12abc3556]
    Former-commit-id: 0af6717c8af3e3b5e2983c462b53fc8e3d3dbd89

--- .goreleaser_hook.sh
+++ .goreleaser_hook.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
+#
+
+set -ex
 
 go_arch=$1
 go_os=$2
 project_name=$3
 
-# Make Go -> Rust arch/os mapping
 case $go_arch in
     amd64) rust_arch='x86_64' ;;
     arm64) rust_arch='aarch64' ;;
+    armv7) rust_arch='armv7' ;;
     *) echo "unknown arch: $go_arch" && exit 1 ;;
 esac
 case $go_os in
