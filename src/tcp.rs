91d1215d5b4fbeec1abdd7db4d15ce9192d4e022
Σrebe - Romain GERARD <erebe@erebe.eu>
2023-11-09 17:18:38 +0100

    fix(proxy): url decode password of http proxy

--- src/tcp.rs
+++ src/tcp.rs
@@ -112,6 +112,8 @@ pub async fn connect_with_http_proxy(
     info!("Connected to http proxy {}:{}", proxy_host, proxy_port);
 
     let authorization = if let Some((user, password)) = proxy.password().map(|p| (proxy.username(), p)) {
+        let password =
+            urlencoding::decode(password).with_context(|| format!("Cannot urldecode proxy password: {}", password))?;
         let creds = base64::engine::general_purpose::STANDARD.encode(format!("{}:{}", user, password));
         format!("Proxy-Authorization: Basic {}\r\n", creds)
     } else {
