532e0e8396ab63140b7aa972c5e413df2ecd0b56
Markus Ongyerth <ongyerth@google.com>
2025-01-07 10:50:08 +0100

    Only serve metrics under /metrics

--- src/metrics/mod.rs
+++ src/metrics/mod.rs
@@ -1,7 +1,7 @@
 use bytes::Bytes;
 use http_body_util::Full;
 use hyper::service::service_fn;
-use hyper::{body, Request, Response, Version};
+use hyper::{body, Request, Response, StatusCode, Version};
 use hyper_util::rt::TokioExecutor;
 
 use opentelemetry_sdk::metrics::SdkMeterProvider;
@@ -38,25 +38,36 @@ pub async fn setup_metrics_provider(addr: &str) -> anyhow::Result<SdkMeterProvid
                 .serve_connection(
                     stream,
                     service_fn(|req: Request<body::Incoming>| {
-                        // Create handler local registry for ownership
-                        let registry = registry.clone();
-                        async move {
-                            let encoder = TextEncoder::new();
-                            let metric_families = registry.gather();
-                            let mut result = Vec::new();
-                            if let Err(err) = encoder.encode(&metric_families, &mut result) {
-                                error!("Failed to encode prometheus metrics: {:?}", err);
-                                return Err("failed to create metrics export")
-                            }
-
-                            if req.version() == Version::HTTP_11 {
-                                Ok(Response::new(Full::<Bytes>::from(result)))
-                            } else {
-                                // Note: it's usually better to return a Response
-                                // with an appropriate StatusCode instead of an Err.
-                                Err("not HTTP/1.1, abort connection")
-                            }
+                        if req.uri().path() != "/metrics" {
+                            return std::future::ready(
+                                Response::builder()
+                                    .status(StatusCode::NOT_FOUND)
+                                    .body(Default::default()),
+                            );
                         }
+
+                        if req.version() != Version::HTTP_11 {
+                            return std::future::ready(
+                                Response::builder()
+                                    .status(StatusCode::BAD_REQUEST)
+                                    .body(Full::<Bytes>::from("Failed to generate metrics")),
+                            );
+                        }
+
+                        // Create handler local registry for ownership
+                        let encoder = TextEncoder::new();
+                        let metric_families = registry.gather();
+                        let mut result = Vec::new();
+                        if let Err(err) = encoder.encode(&metric_families, &mut result) {
+                            error!("Failed to encode prometheus metrics: {:?}", err);
+                            return std::future::ready(
+                                Response::builder()
+                                    .status(StatusCode::INTERNAL_SERVER_ERROR)
+                                    .body(Full::<Bytes>::from("Failed to generate metrics")),
+                            );
+                        }
+
+                        std::future::ready(Ok(Response::new(Full::<Bytes>::from(result))))
                     }),
                 )
                 .await;
